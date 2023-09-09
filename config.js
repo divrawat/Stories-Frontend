2b1d76b70f489839cccbcdacbe18a70522b65294
Divyanshu rawat <divrawat2001@gmail.com>
2023-09-09 17:19:20 +0530

    546

--- config.js
+++ config.js
@@ -1,28 +1,17 @@
 
-const publicRuntimeConfig = {
-    APP_NAME: 'Stories',
-    APP_DESCRIPTION:'', 
-    API_DEVELOPMENT: 'http://localhost:8000/api',
-    MY_API:"http://localhost:8000",
-    API_PRODUCTION:"",
-    PRODUCTION: false,
-    DOMAIN_DEVELOPMENT:"http://localhost:3000",
-    DOMAIN_PRODUCTION:"",
-    }
 
-
-    /*
+    
     const publicRuntimeConfig = {
-        APP_NAME: 'Stories',
+        APP_NAME: 'MyStories',
         APP_DESCRIPTION:'Stories is a news website that delivers content on the basis of web stories.', 
-        API_DEVELOPMENT: 'https://stories.naukarisamachar.live/api,
+        API_DEVELOPMENT: 'https://stories-backend.onrender.com/api',
         MY_API:"http://localhost:8000",
-        API_PRODUCTION:"",
+        API_PRODUCTION:"https://stories-backend.onrender.com/api",
         PRODUCTION: true,
-        DOMAIN_DEVELOPMENT:"https://stories.naukarisamachar.live",
-        DOMAIN_PRODUCTION:"",
+        DOMAIN_DEVELOPMENT:"http://localhost:3000",
+        DOMAIN_PRODUCTION:"https://stories-frontend-zeta.vercel.app/",
         }
-        */
+        
 
     export const MY_API=publicRuntimeConfig.MY_API;
 
@@ -37,4 +26,15 @@ export const DOMAIN = publicRuntimeConfig.PRODUCTION
     ? publicRuntimeConfig.DOMAIN_PRODUCTION
     : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
 
-    
+    /*
+const publicRuntimeConfig = {
+    APP_NAME: 'Stories',
+    APP_DESCRIPTION:'', 
+    API_DEVELOPMENT: 'http://localhost:8000/api',
+    MY_API:"http://localhost:8000",
+    API_PRODUCTION:"",
+    PRODUCTION: false,
+    DOMAIN_DEVELOPMENT:"http://localhost:3000",
+    DOMAIN_PRODUCTION:"",
+    }
+*/
