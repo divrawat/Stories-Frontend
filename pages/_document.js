516af7907d460afbdc1a2ce50aaae325f3fa0897
Divyanshu rawat <divrawat2001@gmail.com>
2023-09-09 23:44:10 +0530

    42sf

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,88 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en-US">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style amp-custom="" dangerouslySetInnerHTML={{
+            __html: ` amp-story-page {
+              background-image: url("/back.webp");
+            }
+        amp-story-grid-layer{
+          background-image: linear-gradient(.5turn,rgba(0,0,0,0) 0%,rgba(0,0,0,.7) 99%);
+        }
+        amp-story-grid-layer{
+          margin: auto;
+        }
+        amp-story-page h1{
+          font-weight: 800;
+          font-size: 28px;
+          line-height: 1.5;
+          color: white;
+          text-transform: capitalize;
+          font-family:Arial, Helvetica, sans-serif;
+          padding-left: 20px;
+          padding-right: 20px;
+          padding-bottom: 25px;
+          word-wrap: break-word;
+        }
+        amp-story-page  h3 {
+          font-weight: 800;
+          font-size: 28px;
+          line-height: 1.5;
+          color: white;
+          text-transform: capitalize;
+          font-family:Arial, Helvetica, sans-serif;
+          padding-left: 20px;
+          padding-right: 20px;
+          padding-bottom: 45px;
+          word-wrap: break-word;
+        
+        }
+        amp-story-page h2 {
+          font-weight: 800;
+          font-size: 30px;
+          line-height: 1.5;
+          color: white;
+          text-transform: capitalize;
+          font-family:Arial, Helvetica, sans-serif;
+          text-align: center;
+          padding-left: 20px;
+          padding-right: 20px;
+          word-wrap: break-word;
+        }
+        amp-story-page p {
+          padding-left: 20px;
+          padding-right: 20px;
+          font-weight: 700;
+          font-size: 20px;
+          line-height: 1.6;
+          color: white;
+          font-family:Arial, Helvetica, sans-serif;
+        }
+        amp-story-grid-layer.bottom {
+          align-content: end;
+          padding-bottom: 60px;
+        }
+        .myclass amp-story-grid-layer.bottom{
+          padding-bottom: 40px;
+        }
+        amp-story-grid-layer.noedge {
+          padding: 0px;
+        }
+        amp-story-grid-layer.center-text {
+          align-content: center;
+        }
+        amp-img{
+        height: 40vh;
+        }`
+          }} />
+        </>
+      )
+    };
+  }
 }
