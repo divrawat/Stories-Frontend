067314d12251472f474f9db7eb28afe6b12bf30f
Divyanshu rawat <divrawat2001@gmail.com>
2023-09-11 02:11:35 +0530

    saefa5f4

--- pages/web-stories/[slug].js
+++ pages/web-stories/[slug].js
@@ -209,16 +209,16 @@ const Stories = ({ story, errorCode }) => {
 
 
 
-        <amp-story-page ad id="000" >
+        <amp-story-page ad="" aria-hidden="false" distance="0" id="i-amphtml-ad-page-1" class="i-amphtml-element i-amphtml-layout-container i-amphtml-built i-amphtml-layout i-amphtml-story-page-loaded" i-amphtml-layout="container" role="region" xdomain-ad="" i-amphtml-return-to="page5" i-amphtml-advance-to="page6" auto-advance-to="page6" i-amphtml-visited="" active="">
 
-        <amp-story-grid-layer template="fill">
-          <amp-ad type="adsense" template="fill" data-ad-client="ca-pub-1721485376950080" data-ad-slot="6021303939" layout="responsive" amp-story="" width="720" height="1280" data-amp-slot-index="0" data-a4a-upgrade-type="amp-ad-network-adsense-impl">
-          </amp-ad>
+          <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container">
 
-          <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container"><div class="i-amphtml-glass-pane"></div></amp-story-grid-layer>
-          <div class="i-amphtml-story-spinner" aria-hidden="true" aria-label="Loading video"><div class="i-amphtml-story-spinner-container"><div class="i-amphtml-story-spinner-layer"><div class="i-amphtml-story-spinner-circle-clipper left"></div><div class="i-amphtml-story-spinner-circle-clipper right"></div></div></div></div>
+            <amp-ad type="adsense" data-ad-client="ca-pub-1721485376950080" data-ad-slot="6021303939" class="i-amphtml-story-ad i-amphtml-element i-amphtml-layout-fill i-amphtml-layout-size-defined i-amphtml-built i-amphtml-layout" layout="fill" amp-story="" i-amphtml-layout="fill" data-amp-slot-index="1" data-a4a-upgrade-type="amp-ad-network-adsense-impl" ></amp-ad>
 
-        </amp-story-grid-layer>
+            <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container"><div class="i-amphtml-glass-pane"></div></amp-story-grid-layer>
+            <div class="i-amphtml-story-spinner" aria-hidden="true" aria-label="Loading video"><div class="i-amphtml-story-spinner-container"><div class="i-amphtml-story-spinner-layer"><div class="i-amphtml-story-spinner-circle-clipper left"></div><div class="i-amphtml-story-spinner-circle-clipper right"></div></div></div></div>
+
+          </amp-story-grid-layer>
 
 
         </amp-story-page>
