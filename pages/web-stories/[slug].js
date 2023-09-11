68a892eb580520fb48df78e7743838decbe0c375
Divyanshu rawat <divrawat2001@gmail.com>
2023-09-11 22:37:37 +0530

    5sdvs8fwf

--- pages/web-stories/[slug].js
+++ pages/web-stories/[slug].js
@@ -221,7 +221,7 @@ const Stories = ({ story, errorCode }) => {
         </amp-story-page> */}
 
 
-{story.slides.map((slide, i) => (
+        {story.slides.map((slide, i) => (
           <>
             <amp-story-page id={`page${i}`} key={i} auto-advance-after="5s">
               <amp-story-grid-layer template="vertical">
@@ -255,16 +255,15 @@ const Stories = ({ story, errorCode }) => {
 
 
             {i === 4 ? (
-              <amp-story-page id="i-amphtml-ad-page-1" class="i-amphtml-element i-amphtml-layout-container i-amphtml-built i-amphtml-layout i-amphtml-story-page-loaded" i-amphtml-layout="container" role="region" xdomain-ad="" i-amphtml-return-to="page4" i-amphtml-advance-to="page5" auto-advance-to="page5" i-amphtml-visited="" active="">
+              <amp-story-page id="i-amphtml-ad-page-1" i-amphtml-layout="container" role="region" xdomain-ad="" i-amphtml-return-to="page4" auto-advance-to="page5">
 
                 <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container">
 
                   <amp-ad type="adsense" data-ad-client="ca-pub-1721485376950080" data-ad-slot="6021303939" class="i-amphtml-story-ad i-amphtml-element i-amphtml-layout-fill i-amphtml-layout-size-defined i-amphtml-built i-amphtml-layout" layout="fill" amp-story="" i-amphtml-layout="fill" data-amp-slot-index="1" data-a4a-upgrade-type="amp-ad-network-adsense-impl" ></amp-ad>
-
-                  <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container"><div class="i-amphtml-glass-pane"></div></amp-story-grid-layer>
-                  <div class="i-amphtml-story-spinner" aria-hidden="true" aria-label="Loading video"><div class="i-amphtml-story-spinner-container"><div class="i-amphtml-story-spinner-layer"><div class="i-amphtml-story-spinner-circle-clipper left"></div><div class="i-amphtml-story-spinner-circle-clipper right"></div></div></div></div>
-
                 </amp-story-grid-layer>
+
+                <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container"><div class="i-amphtml-glass-pane"></div></amp-story-grid-layer>
+                <div class="i-amphtml-story-spinner" aria-hidden="true" aria-label="Loading video"><div class="i-amphtml-story-spinner-container"><div class="i-amphtml-story-spinner-layer"><div class="i-amphtml-story-spinner-circle-clipper left"></div><div class="i-amphtml-story-spinner-circle-clipper right"></div></div></div></div>
               </amp-story-page>
             ) : null}
 
