9836c0357c8fe6d742df7a1bec33b3d5e51704d9
Divyanshu rawat <divrawat2001@gmail.com>
2023-09-12 15:34:02 +0530

    5s5s4

--- pages/web-stories/[slug].js
+++ pages/web-stories/[slug].js
@@ -226,8 +226,8 @@ const Stories = ({ story, errorCode }) => {
         <amp-story-page ad="" id="ads" i-amphtml-layout="container" >
           <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container">
 
-            {/* <amp-ad type="adsense" data-ad-client="ca-pub-1721485376950080" data-ad-slot="6021303939" layout="fill" data-amp-slot-index="1" data-a4a-upgrade-type="amp-ad-network-adsense-impl" ></amp-ad> */}
-            <amp-story-auto-ads></amp-story-auto-ads>
+            <amp-ad type="adsense" data-ad-client="ca-pub-1721485376950080" data-ad-slot="6021303939" layout="fill" data-amp-slot-index="1" data-a4a-upgrade-type="amp-ad-network-adsense-impl" ></amp-ad>
+
           </amp-story-grid-layer>
 
           <amp-story-grid-layer template="fill" class="i-amphtml-element i-amphtml-layout-container i-amphtml-story-layer i-amphtml-built i-amphtml-layout" i-amphtml-layout="container"><div class="i-amphtml-glass-pane"></div></amp-story-grid-layer>
