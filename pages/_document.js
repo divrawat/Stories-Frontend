6b67bbdf69a1a3e3dfef0adb20875c34c32dad5a
Divyanshu rawat <divrawat2001@gmail.com>
2023-09-13 16:01:53 +0530

    sd5f4s56v

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
