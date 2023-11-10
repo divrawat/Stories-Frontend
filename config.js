a71c03d9707e0759c53db73b5ad2ad720f85b516
divrawat <divrawat2001@gmail.com>
2023-11-10 15:15:09 +0530

    sdv00

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
-  // export const API = "https://stories-backend-six.vercel.app/api";
-//  export const DOMAIN = "https://stories.liquorprices.in";
+  export const API = "https://stories-backend-six.vercel.app/api";
+  export const DOMAIN = "https://stories.liquorprices.in";
 
-   export const API = "http://localhost:8000/api";
-    export const DOMAIN = "http://localhost:3000"
+  //  export const API = "http://localhost:8000/api";
+    // export const DOMAIN = "http://localhost:3000"
 
 export const APP_NAME = 'Stories';
 export const MY_API="https://stories-backend-six.vercel.app";
